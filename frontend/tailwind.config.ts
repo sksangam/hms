f1ae2a7d604deb5c1ad6b07b697345c2ac7e40e7
SUBHASH KUMAR <subhashsangam@gmail.com>
2025-01-15 13:39:54 +0530

    initialized project using nextjs with mantine and tailwind css in frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+import { breakpoints, colors } from "./src/app/theme";
+import tailwindPresetMantine from "tailwind-preset-mantine";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  presets: [
+    tailwindPresetMantine({
+      mantineBreakpoints: breakpoints,
+      mantineColors: colors,
+    }),
+  ],
+  plugins: [],
+};
+export default config;
